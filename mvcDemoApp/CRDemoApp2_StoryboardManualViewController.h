2b4006fc26ec72b412a7c25b0a7c3de6080f9d86
Chris Ricca <christopher.ricca@gmail.com>
2014-04-15 17:15:04 -0400

    partially completed manual storyboard

---
+++ mvcDemoApp/CRDemoApp2_StoryboardManualViewController.h
@@ -0,0 +1,13 @@
+//
+//  CRDemoApp2_StoryboardManualViewController.h
+//  mvcDemoApp
+//
+//  Created by Chris Ricca on 4/15/14.
+//  Copyright (c) 2014 Chris Ricca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CRDemoApp2_StoryboardManualViewController : UIViewController
+
+@end
---
+++ mvcDemoApp/CRDemoApp2_StoryboardManualViewController.m
@@ -0,0 +1,38 @@
+//
+//  CRDemoApp2_StoryboardManualViewController.m
+//  mvcDemoApp
+//
+//  Created by Chris Ricca on 4/15/14.
+//  Copyright (c) 2014 Chris Ricca. All rights reserved.
+//
+
+#import "CRDemoApp2_StoryboardManualViewController.h"
+
+@interface CRDemoApp2_StoryboardManualViewController ()
+
+@end
+
+@implementation CRDemoApp2_StoryboardManualViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
